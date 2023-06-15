be5ded7fe2d9c770dc0e5389ea06c752ba820943
Zoya <byzyka1986@mail.ru>
2023-06-15 23:15:48 +0300

    Домашнее задание к лекции 1 Основные понятия

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,28 @@
+
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = Math.pow(b, 2)-4*a*c;
+
+  if (discriminant === 0) {
+    let root = -b/(2*a);
+    arr.push(root);
+  }  else if(discriminant > 0) {
+    let root = (-b + Math.sqrt(discriminant) )/(2*a);
+    arr.push(root);
+    root = (-b - Math.sqrt(discriminant) )/(2*a);
+    arr.push(root);
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = percent/100;
+  let interestRate = percent/12;
+  let credit = amount - contribution;
+  let payment = credit * (interestRate + (interestRate / (Math.pow((1 + interestRate), countMonthsn) - 1)));
+  let sum = payment * countMonths;
+
+  return +sum.toFixed(2);
 }
