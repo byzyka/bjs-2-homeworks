cf1406e430c2028a7c24926399a892fb327c5148
Zoya <byzyka1986@mail.ru>
2023-06-27 20:45:35 +0300

    Домашнее задание к лекции Объекты

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,31 @@
 function compareArrays(arr1, arr2) {
-  
+    if(arr1.length === arr2.length &&
+        arr1.every((value, index) => value === arr2[index])) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+ let result = users.
+ filter(user => user.gender == gender).map(user => user.age).reduce((acc, age, i, arr) => acc + age / arr.length, 0)
+
+
+    /*let male = users.filter(user => user.gender == "мужской");
+    let famale = users.filter(user => user.gender == "женский");
+      
+    let maleAge = male.map(user => user.age);
+    let famaleAge = famale.map(user => user.age);
+    
+       
+    let totalMale = maleAge.reduce((acc, age) => acc + age / maleAge.length, 0);
+    let totalFamale = famaleAge.reduce((acc, age) => acc + age / famaleAge.length, 0);
+
+    console.log(totalFamale);*/
+
+    return result;
 }
+   
+
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks =[];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+   this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if(this.hasOwnProperty('marks') === true) {
+        for(let i = 0; i < marks.length; i++) {
+           this.marks.push(marks[i]);  
+        } 
+    }
 }
 
-Student.prototype.getAverage = function () {
-  
-}
+Student.prototype.getAverage = function (...marks) {   
+    if('marks' in this != true || this.marks.length == 0) {
+        return 0;
+    } else {
+        return this.marks.reduce((acc, number) => acc + number, 0) / this.marks.length;
+    }
+} 
 
 Student.prototype.exclude = function (reason) {
-  
+    this.excluded = reason; 
+    delete this.marks;
+    delete this.subject;
+
+    if(this.hasOwnProperty('marks') === 'undefined' || this.hasOwnProperty('subject') === 'undefined') {
+        return reason;
+    }     
 }
