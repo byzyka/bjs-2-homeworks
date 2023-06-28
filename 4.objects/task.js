b218d11095b847663fccd47e90661530c54442eb
Zoya <byzyka1986@mail.ru>
2023-06-28 11:46:44 +0300

    Объекты, домашнее задание

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -11,9 +11,7 @@ Student.prototype.setSubject = function (subjectName) {
 
 Student.prototype.addMarks = function (...marks) {
     if(this.hasOwnProperty('marks') === true) {
-        for(let i = 0; i < marks.length; i++) {
-           this.marks.push(marks[i]);  
-        } 
+        this.marks.push(...marks);  
     }
 }
 
@@ -30,7 +28,5 @@ Student.prototype.exclude = function (reason) {
     delete this.marks;
     delete this.subject;
 
-    if(this.hasOwnProperty('marks') === 'undefined' || this.hasOwnProperty('subject') === 'undefined') {
-        return reason;
-    }     
+    this.hasOwnProperty('marks') === 'undefined' || this.hasOwnProperty('subject') === 'undefined'
 }
